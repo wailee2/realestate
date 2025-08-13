24cd083e872a19dc83f905255ef2802b11834d9d
Feranmi <9fernni@gmail.com>
2025-08-13 23:21:31 +0100

    fixed vercel refresh

--- src/home/About.jsx
+++ src/home/About.jsx
@@ -30,7 +30,7 @@ export default function About() {
                 </div>
             </div>
             <div className="relative w-full mt-5">
-                <div className="relative min-h-[100vh] md:min-h-[115vh]">
+                <div className="relative min-h-[5vh] md:min-h-[115vh]">
                     <div className=" lg:absolute right-0 space-y-5">
                         <div className="flex lg:gap-8">
                             <div>
